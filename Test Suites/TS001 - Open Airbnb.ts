863c6eb4f9da48fb02e497fdfcc9ec53fc884ba5
snugroho <snugroho@FA-2209031-1-1.intikom.dc>
2025-01-14 12:00:08 +0700

    first commit

---
+++ Test Suites/TS001 - Open Airbnb.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS001 - Open Airbnb</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f737a5d0-da6f-4c4c-bdf9-6b60108c3970</testSuiteGuid>
+   <testCaseLink>
+      <guid>4ff48d94-e0c0-421b-b3a3-5d6620f4d377</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC001 - Open Airbnb</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
